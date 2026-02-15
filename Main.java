20ee1a3b651f817d77871d3b442adc99c02b623e
dhanuka2004 <dhanukadilsara@gmail.com>
2026-02-15 21:57:24 +0530

    Initial commit - basic SIS structure

---
+++ Main.java
@@ -0,0 +1,77 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StudentManager manager = new StudentManager();
+
+        while (true) {
+            System.out.println("\n--- Student Information System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine(); // consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Age: ");
+                    int age = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Course: ");
+                    String course = sc.nextLine();
+
+                    manager.addStudent(new Student(id, name, age, course));
+                    break;
+
+                case 2:
+                    manager.viewStudents();
+                    break;
+
+                case 3:
+                    System.out.print("Enter ID to search: ");
+                    int searchId = sc.nextInt();
+                    Student s = manager.searchStudent(searchId);
+                    if (s != null) System.out.println(s);
+                    else System.out.println("Student not found!");
+                    break;
+
+                case 4:
+                    System.out.print("Enter ID to update: ");
+                    int updateId = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter New Name: ");
+                    String newName = sc.nextLine();
+                    System.out.print("Enter New Age: ");
+                    int newAge = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter New Course: ");
+                    String newCourse = sc.nextLine();
+
+                    manager.updateStudent(updateId, newName, newAge, newCourse);
+                    break;
+
+                case 5:
+                    System.out.print("Enter ID to delete: ");
+                    int deleteId = sc.nextInt();
+                    manager.deleteStudent(deleteId);
+                    break;
+
+                case 6:
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
+}
